1b27acb46dda595b7d23b511564e6ba94f0ed6ed
peng zhihua <pengzh200083@3irobotics.com>
2024-04-30 18:17:08 +0800

    init.

---
+++ entry/src/main/ets/commons/StatusBarManager.ts
@@ -0,0 +1,53 @@
+import window from '@ohos.window'
+
+export class StatusBarManager {
+  private static mInstance: StatusBarManager
+  private mWindowStage: window.WindowStage
+
+  private constructor() { }
+
+  public static on(windowStage: window.WindowStage = undefined): StatusBarManager {
+    if (!this.mInstance) {
+      this.mInstance = new StatusBarManager()
+    }
+    if (windowStage) {
+      this.mInstance.mWindowStage = windowStage
+    }
+    return this.mInstance
+  }
+
+  /**
+   * 设置沉浸式状态栏
+   * @param is 是否沉浸式，默认值 true
+   * @param arrSystemBarEnable 导航栏、状态栏的可见模式
+   * @param windowStage WindowStage实例
+   * @returns 状态栏高度
+   */
+  public setImmersiveStatusBar(
+    is: boolean = true,
+    arrSystemBarEnable: Array<'status'|'navigation'> = ['status', 'navigation'],
+    windowStage: window.WindowStage = this.mWindowStage
+  ): Promise<{statusBarHeight: number, navigationBarHeight: number}> {
+    return new Promise((resolve, reject) => {
+      windowStage.getMainWindow().then(windowClass => {
+        windowClass.setWindowLayoutFullScreen(is).then(() => {
+          if (is) {
+            windowClass.setWindowSystemBarEnable(arrSystemBarEnable).then(() => {
+              const avoidArea = windowClass.getWindowAvoidArea(window.AvoidAreaType.TYPE_SYSTEM)
+              // TODO: setWindowSystemBarProperties API9未生效
+              windowClass.setWindowSystemBarProperties({
+                statusBarColor: '#00000000',
+                statusBarContentColor: '#ffffff',
+                navigationBarColor: '#00000000',
+                navigationBarContentColor:'#ffffff'
+              })
+              resolve({statusBarHeight: avoidArea.topRect.height | 0, navigationBarHeight: avoidArea.bottomRect.height | 0})
+            }).catch(err => reject(err))
+          } else {
+            resolve({statusBarHeight: 0, navigationBarHeight: 0})
+          }
+        }).catch(err => reject(err))
+      }).catch(err => reject(err))
+    })
+  }
+}
---
+++ entry/src/main/ets/commons/Utils.ts
@@ -0,0 +1,6 @@
+export class Utils {
+  static toLowerCase(str: string){
+    return str.toLowerCase();
+  }
+
+}
